92960beeb3ac11b36f6e6d7d8312ae69092be7c3
abubakrali10 <bakrali175@yahoo.com>
2023-05-25 15:39:19 -0700

    pop function to remove element from the top of the stack added

--- pint_pop.c
+++ pint_pop.c
@@ -15,3 +15,25 @@ void pint(stack_t **stack, unsigned int line_number)
 	}
 	printf("%d\n", (*stack)->n);
 }
+
+/**
+ * pop - removes top element from stack
+ * @stack: pointer to stack
+ * @line_number: line number in file
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	ptr = *stack;
+	*stack = (*stack)->next;
+
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(ptr);
+}
