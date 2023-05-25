b9760f29b87ead322276073356e469bbdd0d5687
abubakrali10 <bakrali175@yahoo.com>
2023-05-25 14:37:17 -0700

    header file updated - pint function prototype added

--- monty.h
+++ monty.h
@@ -39,6 +39,7 @@ typedef struct instruction_s
 instruction_t *check_opcode(char *opcode);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 int check_num(const char *str, int *num);
 void free_stack(stack_t **stack);
 
