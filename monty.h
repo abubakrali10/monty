01ee4d289ab02bb97b07043dd4ce1b187f28ba4b
abubakrali10 <bakrali175@yahoo.com>
2023-05-25 16:17:13 -0700

    header file function prototypes updated

--- monty.h
+++ monty.h
@@ -42,6 +42,7 @@ void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 int check_num(const char *str, int *num);
 void free_stack(stack_t **stack);
 
