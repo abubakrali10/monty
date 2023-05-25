f81c5b68a03a944f1063e439293bfc484d81aeff
abubakrali10 <bakrali175@yahoo.com>
2023-05-25 14:09:33 -0700

    function to get monty instruction from bytecode

---
+++ check_opcode.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * check_opcode - get monty instraction
+ * @opcode: opcode
+ * Return: a pointer to instruction_t or NULL if opcode not found
+ */
+
+instruction_t *check_opcode(char *opcode)
+{
+	int i = 0;
+	instruction_t *instruction = NULL;
+	instruction_t op_codes[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+	};
+
+	while (op_codes[i].opcode != NULL)
+	{
+		if (strcmp(opcode, op_codes[i].opcode) == 0)
+		{
+			instruction = malloc(sizeof(instruction_t));
+
+			if (instruction == NULL)
+			{
+				fprintf(stderr, "Error: malloc failed\n");
+				exit(EXIT_FAILURE);
+			}
+			memcpy(instruction, &op_codes[i], sizeof(instruction_t));
+			break;
+		}
+		i++;
+	}
+	return (instruction);
+}
