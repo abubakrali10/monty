a52b36e74e780237a85c0630a1a10575152d0a48
abubakrali10 <bakrali175@yahoo.com>
2023-05-25 17:46:58 -0700

    math operations on stack

--- math_funcs.c
+++ math_funcs.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 
 /**
- * add - performing addition of top 2 elements of the stack
+ * _add - performing addition of top 2 elements of the stack
  * @stack: pointer to stack
  * @line_number: line number in file
  */
-void add(stack_t **stack, unsigned int line_number)
+void _add(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
@@ -14,6 +14,95 @@ void add(stack_t **stack, unsigned int line_number)
 	}
 	(*stack)->next->n += (*stack)->n;
 	(*stack)->next->prev = (*stack)->prev;
+	*stack = (*stack)->next;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
+/**
+ * _sub - performing substaction of top 2 elements of the stack
+ * @stack: pointer to stack
+ * @line_number: line number in file
+ */
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n -= (*stack)->n;
+	(*stack)->next->prev = (*stack)->prev;
+	*stack = (*stack)->next;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
+/**
+ * _div - for perfoming division on stack
+ * @stack: pointer to stack
+ * @line_number: line number in file
+ */
+void _div(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n /= (*stack)->n;
+	(*stack)->next->prev = (*stack)->prev;
+	*stack = (*stack)->next;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
+/**
+ * _mul - performing multiplication on top 2 elements of the stack
+ * @stack: pointer to stack
+ * @line_number: line number in file
+ */
+void _mul(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n *= (*stack)->n;
+	(*stack)->next->prev = (*stack)->prev;
+	*stack = (*stack)->next;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
+/**
+ * _mod - performing mod on top 2 elements of the stack
+ * @stack: pointer to stack
+ * @line_number: line number in file
+ */
+void _mod(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n %= (*stack)->n;
 	*stack = (*stack)->next;
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
